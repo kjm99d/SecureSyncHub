7e577def3c2d0881a1f512857f0901543c100b0e
Jungmin Kim <kjm99d@naver.com>
2024-09-16 02:33:57 +0900

    ✨ 로그인 시 JWT 토큰 발급 기능 추가

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,10 @@
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import Models from '../models/index.js';
 const { User } = Models;
+
+import Jwt from '../config/jwt.js'; // 환경 변수 설정
+const { JWT_SECRET, JWT_EXPIRATION } = Jwt;
 
 // 회원가입
 const registerUser = async (req, res) => {
@@ -14,9 +18,9 @@ const registerUser = async (req, res) => {
 };
 
 // 로그인
-const loginUser = async (req, res) => {
+export const loginUser = async (req, res) => {
   try {
-    const { username, password, email } = req.body;
+    const { username, password } = req.body;
 
     const user = await User.findOne({ where: { username } });
 
@@ -25,28 +29,55 @@ const loginUser = async (req, res) => {
     }
 
     // 비밀번호 확인
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
+    //const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (password != user.password) {
       return res.status(401).json({ error: 'Invalid password' });
     }
 
+    // JWT 토큰 생성
+    const token = jwt.sign(
+      { id: user.id, username: user.username, role: user.role }, // 토큰에 포함할 정보 (payload)
+      JWT_SECRET, // 비밀키 (서버에만 존재)
+      { expiresIn: JWT_EXPIRATION || '1h' } // 토큰 만료 시간 설정 (기본 1시간)
+    );
+
     // 비밀번호는 제외하고 사용자 정보와 토큰 반환
-    // const { password: _, ...userWithoutPassword } = user.toJSON();
+    const { password: _, ...userWithoutPassword } = user.toJSON();
     res.status(200).json({ user: userWithoutPassword, token });
 
   } catch (error) {
-    res.status(400).json({ error: 'Error creating user' });
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
   }
 };
 
 // 사용자 정책 조회 API
 const findPolicy = async (req, res) => {
   try {
-    const { username, password, email } = req.body;
-    const newUser = await User.create({ username, password, email });
-    res.status(201).json(newUser);
+    // 헤더에서 Authorization 토큰 가져오기
+    const token = req.headers['authorization'];
+
+    if (!token) {
+      return res.status(401).json({ error: 'Token missing' });
+    }
+
+    // 토큰 검증
+    console.log(token, JWT_SECRET);
+    const decoded = jwt.verify(token, JWT_SECRET);
+    console.log("next");
+    // 토큰이 유효하지 않으면 에러 반환
+    if (!decoded) {
+      return res.status(401).json({ error: 'Invalid token' });
+    }
+
+
+
+    res.status(201).json({ "result" : "ok" });
+
   } catch (error) {
-    res.status(400).json({ error: 'Error creating user' });
+
+    res.status(400).json({ error: 'Error find policy error' });
+
   }
 };
 
