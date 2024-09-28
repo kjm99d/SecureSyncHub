9207f5c8b75d717c38bfc68ca79b142d6cf4489a
Jungmin Kim <kjm99d@naver.com>
2024-09-28 23:56:05 +0900

    🔥 EJS 관련 파일 제거

--- index.js
+++ index.js
@@ -11,10 +11,6 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// EJS 템플릿 엔진 설정
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views')); // views 디렉토리 경로 설정
-
 // 정적 파일 제공 (CSS, 이미지 등)
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -30,18 +26,6 @@ app.use('/api/files', fileRoutes);
 
 // 관리자 관련 라우트 연결
 app.use('/api/admin', adminRoutes);  // 관리자 경로
-// ========================================================================== //
-// Page Connect
-// ========================================================================== //
-// 로그인 페이지 렌더링 라우트
-app.get('/', (req, res) => {
-  res.render('login'); // views/login.ejs 파일을 렌더링
-});
-
-// 회원가입 페이지 라우트
-app.get('/register', (req, res) => {
-  res.render('register'); // views/signup.ejs 파일을 렌더링 (회원가입 페이지 추가)
-});
 // ========================================================================== //
 // Start Server
 // ========================================================================== //
